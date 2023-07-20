276dfa3af639d7f5959fb3b2d020e10749fa5b71
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-07-20 23:45:08 +0300

    add argument description

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from app.core.config import settings
 # создание объекта приложения
 # устанавливаем заголовок приложения при помощи аргумента title,
 # в качестве значения укажем аттрибут app_title объекта settings
-app = FastAPI(title=settings.app_title)
-
-#
+app = FastAPI(
+    title=settings.app_title,
+    description=settings.app_description
+)
