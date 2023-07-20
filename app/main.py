76e6502cb6ff3554fa24ef6da7ff641b9a8c649a
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-07-20 19:39:04 +0300

    create main-file with app

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+# импортируем настройки проекта из config.py
+from app.core.config import settings
+
+# создание объекта приложения
+# устанавливаем заголовок приложения при помощи аргумента title,
+# в качестве значения укажем аттрибут app_title объекта settings
+app = FastAPI(title=settings.app_title)
+
+#
