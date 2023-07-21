9471cf752b26153ea203e87b178cef2a47e87b10
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-07-21 18:53:41 +0300

    create model User

---
+++ app/models/user.py
@@ -0,0 +1,10 @@
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+
+from app.core.db import Base
+
+
+# создадим модель User на основе готовой базовой модели пользователя
+# из библиотеки  FastAPI и нашей базовой модели
+class User(SQLAlchemyBaseUserTable[int], Base):
+    """Модель пользователя."""
+    pass
