9f3cfa4c4f0e9a2b9d85711f9956e1cc0185956d
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-07-23 01:19:16 +0300

    add init-file for models dir

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+# чтобы SQLAlchemy узнала обо всех моделях до того, как
+# начнут выстраиваться взаимосвязи между ними, нужно импортировать все модели в файл init
+from .charity_projects import CharityProject # noqa
+from .donation import Donation # noqa
+from .user import User # noqa
