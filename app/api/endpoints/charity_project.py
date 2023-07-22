0914987f4e65036b2e9242267f03e40f521e9e15
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-07-22 17:59:19 +0300

    edited charity_projects endpoints

--- app/api/endpoints/charity_project.py
+++ app/api/endpoints/charity_project.py
@@ -2,15 +2,17 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, Depends
 from app.core.db import get_async_session
 from app.models.charity_projects import CharityProject
-from app.schemas.charity_projects import CharityProjectCreate
+from app.schemas.charity_projects import CharityProjectDB, CharityProjectCreate
 from app.crud.charity_projects import charity_project_crud
+
+
 router = APIRouter()
 
 
 # опишем эндпоинт для операции Create
 @router.post(
     '/charity_project/',
-    response_model=CharityProject)
+    response_model=CharityProjectDB)
 # API-функция, обработчик запроса
 # эта функция будет использовать асинхронную crud-функцию
 # поэтому она и сама должна быть асинхронной
