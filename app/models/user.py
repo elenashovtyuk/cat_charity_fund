eb30bb0590e6375cfa8d6d92548a691b5a7e86f4
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-07-23 16:35:30 +0300

    edited user model

--- app/models/user.py
+++ app/models/user.py
@@ -6,5 +6,5 @@ from app.core.db import Base
 # создадим модель User на основе готовой базовой модели пользователя
 # из библиотеки  FastAPI и нашей базовой модели
 class User(SQLAlchemyBaseUserTable[int], Base):
-    """Модель пользователя."""
+    """Модель пользователей."""
     pass
