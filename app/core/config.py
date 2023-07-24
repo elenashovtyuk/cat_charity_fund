652e629796bfcb91ae80d6d4f500b9fc781d61b0
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-07-24 20:28:59 +0300

    added first superuser

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,12 @@
 from pydantic import BaseSettings
-
-
+from typing import Optional
+from pydantic import BaseSettings, EmailStr
 # для работы с переменными окружения создаем класс,
 # унаследованный от BaseSettings из библиотеки pydantic
 # этот класс позволяет считывать из оперюсистемы переменные окружения
 # напрямую обращаться к файлу .env
+
+
 class Settings(BaseSettings):
     # добавим аттрибут app_title - в нем будет храниться название приложения
     # в аннотации укажем, что это строковый тип данных и зададим значение
@@ -12,6 +14,9 @@ class Settings(BaseSettings):
     app_title: str = 'QRKot'
     app_description: str = 'Сервис для поддержки котиков!'
     database_url: str
+    secret: str = 'SECRET'
+    first_superuser_email: Optional[EmailStr] = None
+    first_superuser_password: Optional[str] = None
 
     # подкласс Config содержит спец.аттрибут env_file
     # в этом аттрибуте укажем имя файла с переменными окружения
