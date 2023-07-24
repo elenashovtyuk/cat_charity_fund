77e621b16abf3e9c446e17ba1b1a73e88017af1f
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-07-25 00:42:56 +0300

    edited charity_project endpoints

--- app/api/endpoints/charity_project.py
+++ app/api/endpoints/charity_project.py
@@ -22,8 +22,7 @@ async def create_new_charity_project(
         charity_project: CharityProjectCreate,
         session: AsyncSession = Depends(get_async_session),
 ):
-
-    """Обработка запроса на создание нового проекта пожертвований."""
+    """Только для суперюзеров."""
     await check_name_duplicate(charity_project.name, session)
     new_charity_project = await charity_project_crud.create(charity_project, session)
     return new_charity_project
@@ -44,14 +43,20 @@ async def get_all_charity_projecs(
 # опишем эндпоинт для операции PATCH
 @router.patch(
     '/{charity_project_id}',
-    response_model=CharityProjectDB
+    # укажем схему для ответа
+    response_model=CharityProjectDB,
+    # укажем зависимость, так как patch-запрос может сделать только суперюзер
+    dependencies=[Depends(current_superuser)]
 )
 async def partially_update_charity_project(
+    # в параметрах корутине передаем
+    # id проекта пожертвования, который нужно изменить
     charity_project_id: int,
-    # JSON-данные, переданные пользователем
+    # JSON-данные, переданные пользователем для изменения объекта
     obj_in: CharityProjectUpdate,
     session: AsyncSession = Depends(get_async_session)
 ):
+    """Только для суперюзеров."""
     # перед тем, как внести изменения в конкретный проект
     # нужно убедиться - существует ли указанный проект в БД
     charity_project = await check_charity_project_exists(
@@ -75,7 +80,8 @@ async def partially_update_charity_project(
 # опишем эндпоинт для операции DELETE
 @router.delete(
     '/{charity_project_id}',
-    response_model=CharityProjectDB
+    response_model=CharityProjectDB,
+    dependencies=[Depends(current_superuser)]
 )
 async def remove_charity_project(
     charity_project_id: int,
