bc7f4e7fde531d22fe165ddcfad7eb5814b4571d
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-07-24 20:26:31 +0300

    delete comments

--- app/models/charity_projects.py
+++ app/models/charity_projects.py
@@ -6,12 +6,5 @@ from .base import InvestBaseModel
 
 class CharityProject(InvestBaseModel):
     """Модель проектов для пожертвований."""
-    # поле name, имя проекта.
-    # название проекта не более 100 символов
-    # имя - уникальное
-    # имя - не может быть пустым
     name = Column(String(100), unique=True, nullable=False)
-    # поле description - описание,
-    # обязательное текстовое поле
-    # не менее одного символа
     description = Column(Text, nullable=False)
