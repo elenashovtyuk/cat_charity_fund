f43f80f1dbf98159115129d96f0ba659a6ed5f7b
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-07-26 18:52:06 +0300

    delete comments, sorted imports

--- alembic/env.py
+++ alembic/env.py
@@ -2,44 +2,24 @@ import asyncio
 import os
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from dotenv import load_dotenv
+from sqlalchemy import engine_from_config, pool
 from sqlalchemy.ext.asyncio import AsyncEngine
 
-from app.core.base import Base
-from dotenv import load_dotenv
 from alembic import context
+from app.core.base import Base
 
-# загрузим файл .env в переменные окружения
-# библиотека python-dotenv умеет находить файл в вышестоящих каталогах,
-# поэтому полный путь указывать не обязательно
+
 load_dotenv('.env')
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-
-# Установим для переменной sqlalchemy.url значение из нашего .env файла.
-# после этого Alembic будет подключаться к той базе данных,
-# которая указана в .env, а чувствительные данные будут в безопасности
 config.set_main_option('sqlalchemy.url', os.environ['DATABASE_URL'])
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline():
