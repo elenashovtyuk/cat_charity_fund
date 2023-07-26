20ed498b3f915cd329ea0f39ba081a44ccb3c2d0
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-07-26 18:14:19 +0300

    delete my comments, sorted imports

--- app/core/config.py
+++ app/core/config.py
@@ -1,16 +1,10 @@
-from pydantic import BaseSettings
 from typing import Optional
+
 from pydantic import BaseSettings, EmailStr
-# для работы с переменными окружения создаем класс,
-# унаследованный от BaseSettings из библиотеки pydantic
-# этот класс позволяет считывать из оперюсистемы переменные окружения
-# напрямую обращаться к файлу .env
 
 
 class Settings(BaseSettings):
-    # добавим аттрибут app_title - в нем будет храниться название приложения
-    # в аннотации укажем, что это строковый тип данных и зададим значение
-    # по умолчанию
+    """Класс с настройками приложения."""
     app_title: str = 'QRKot'
     app_description: str = 'Сервис для поддержки котиков!'
     database_url: str = 'sqlite+aiosqlite:///./qrkot.db'
@@ -18,13 +12,8 @@ class Settings(BaseSettings):
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
 
-    # подкласс Config содержит спец.аттрибут env_file
-    # в этом аттрибуте укажем имя файла с переменными окружения
     class Config:
         env_file = '.env'
 
 
-# создаем глобальную переменную settings с экземпляром класса Settings
-# чтобы его можно было импортировать в любую часть приложения, где
-# потребуется доступ к настройкам
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,22 +1,18 @@
 from fastapi import FastAPI
-from app.core.init_db import create_first_superuser
-# импортируем настройки проекта из config.py
-from app.core.config import settings
-from app.api.routers import main_router
 
-# создание объекта приложения
-# устанавливаем заголовок приложения при помощи аргумента title,
-# в качестве значения укажем аттрибут app_title объекта settings
+from app.api.routers import main_router
+from app.core.config import settings
+from app.core.init_db import create_first_superuser
+
 app = FastAPI(
     title=settings.app_title,
     description=settings.app_description
 )
 
-# подключаем главный роутер
+
 app.include_router(main_router)
 
 
-# При старте приложения запускаем корутину create_first_superuser.
 @app.on_event('startup')
 async def startup():
     await create_first_superuser()
