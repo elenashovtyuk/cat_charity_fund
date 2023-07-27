029d02020017f6090b2752d8bd4756a2f44dd56c
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-07-27 13:47:20 +0300

    delete .vscode

--- app/models/charity_project.py
+++ app/models/charity_project.py
@@ -1,9 +1,10 @@
 from sqlalchemy import Column, String, Text
 
 from .base import InvestBaseModel
+from app.constants import MAX_LENGTH
 
 
 class CharityProject(InvestBaseModel):
     """Модель проектов для пожертвований."""
-    name = Column(String(100), unique=True, nullable=False)
+    name = Column(String(MAX_LENGTH), unique=True, nullable=False)
     description = Column(Text, nullable=False)
